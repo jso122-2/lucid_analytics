e93aeae28d56da9fbca35056cd97290b21f963df
unknown <jacksondev122@gmail.com>
2025-03-05 19:28:03 +1100

    changes final 3

--- blueprints/about.py
+++ blueprints/about.py
@@ -1,10 +1,8 @@
 from flask import Blueprint, render_template
-import pandas as pd
-
 
 about_bp = Blueprint('about', __name__, template_folder='../templates')
 
-@about_bp.route('/about')
+# This route will match both '/about' and '/about/' without redirecting
+@about_bp.route('/', strict_slashes=False)
 def about():
-    # Render the graphing and about section.
     return render_template('about.html')
